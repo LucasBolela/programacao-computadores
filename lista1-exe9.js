f8fc56ed9605acc6472f0b5420896527dce394cd
Lucas <lucasbolelasilveira@gmail.com>
2020-08-26 21:52:30 -0300

    Exe 9 Complete

--- lista1-exe9.js
+++ lista1-exe9.js
@@ -1,1 +1,19 @@
 console.log(`Exercicio 9`)
+
+const boss = () => {
+  let number= Number(prompt(`Digite um numero:`)),
+  soma = ex9(number)
+  console.log(`Soma dos divisores de ${number}: ${soma}`)
+}
+
+const ex9 = number => {
+  let i = number,
+  sum = [];
+  while(i>0){
+    if(number%i==0)
+      sum.push(i)
+    i--
+  }
+  return sum.reduce((total, num)=> total + num, 0)
+}
+boss()
